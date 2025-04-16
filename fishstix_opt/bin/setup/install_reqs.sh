92a439bca4681181d2f6843f8cca807d0b3263e2
Kate Lawrence-Gupta <39601928+klawrencegupta-splunk@users.noreply.github.com>
2025-04-16 14:07:55 -0400

    updates

--- fishstix_opt/bin/setup/install_reqs.sh
+++ fishstix_opt/bin/setup/install_reqs.sh
@@ -31,4 +31,4 @@ sudo snap install microk8s --classic --channel=1.32/stable
 sudo usermod -a -G microk8s splunker
 sudo chown -f -R splunker ~/.kube
 sudo snap alias microk8s.kubectl kubectl
-echo "logout now and back in to continue setup with setup_fishstix.sh""
+echo "logout now and back in to continue setup with setup_fishstix.sh"
