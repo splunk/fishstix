e66e64ecdf74d8614d85f3ab9f00ed85ab603a87
Kate Lawrence-Gupta <39601928+klawrencegupta-splunk@users.noreply.github.com>
2025-04-16 11:35:58 -0400

    Update install_reqs.sh
    updates to fix typos

--- fishstix_opt/bin/setup/install_reqs.sh
+++ fishstix_opt/bin/setup/install_reqs.sh
@@ -14,10 +14,10 @@ fi
 
 #Install the additional required software
 echo "Install Docker, Redis server, pip redis"
-apt install docker.io
-apt install redis-server
-apt install redis-tools
-pip install redis splunklib
+sudo apt install docker.io
+sudo apt install redis-server
+sudo apt install redis-tools
+sudo pip install redis splunklib
 
 #Modify the redis.conf file
 echo "Redis-Server needs to be configured locally"
@@ -31,4 +31,4 @@ sudo snap install microk8s --classic --channel=1.32/stable
 sudo usermod -a -G microk8s splunker
 sudo chown -f -R splunker ~/.kube
 sudo snap alias microk8s.kubectl kubectl
-echo "logout now and back in to continue setup with setup_fishstix.sh""
+echo "logout now and back in to continue setup with setup_fishstix.sh"
