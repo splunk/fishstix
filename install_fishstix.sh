fc428a519fc759baddfb406b5c640a2f09fba2e4
Kate Lawrence-Gupta <39601928+klawrencegupta-splunk@users.noreply.github.com>
2025-05-19 14:13:11 -0400

    Update install_fishstix.sh

--- install_fishstix.sh
+++ install_fishstix.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
-echo "Installing pre-requsites for FishStix install_reqs.sh"
-sudo sh fishstix_opt/bin/setup/install_reqs.sh
+echo "Installing Microk8s Enviroment: this will FORCE a logout; run the install_fishstix.sh again continue the install"
 sudo sh fishstix_opt/bin/setup/mk8.sh
 
-if test -f ".placeholder"; then
-  echo "Picking up where we left off...setup_fishstix.sh"
+if test -f ".mk8s"; then
+  echo "Installing pre-requsites for FishStix install_reqs.sh"
+  sudo sh fishstix_opt/bin/setup/install_reqs.sh
+  echo "Picking up where we left off...running setup_fishstix.sh"
   sleep 1s
   sudo sh fishstix_opt/bin/setup/setup_fishstix.sh
-else
-  echo "Please run the install_reqs.sh first"
-fi
-
-if test -f "/opt/fishstix/yaml/fxcopier.yaml"; then
-  echo ""
-  sleep 1s
+  echo "Finishing setup - please configure Redis"
   sudo sh fishstix_opt/bin/setup/finish_config.sh
 else
-  echo "Please run the setup_fishstix.sh first"
-fi
+  echo "Please run the mk8s.sh first"
