3fa2d8f9dc826f35afdcfb14ed7aa22beb9cb6c8
Kate Lawrence-Gupta <39601928+klawrencegupta-splunk@users.noreply.github.com>
2025-05-19 14:24:06 -0400

    Update install_reqs.sh

--- fishstix_opt/bin/setup/install_reqs.sh
+++ fishstix_opt/bin/setup/install_reqs.sh
@@ -4,20 +4,25 @@
 # https://github.com/splunk/fishstix
 
 #Requirements
-echo "FishStix requires the following; Splunk, Docker, Redis-Server/Tools & pip redis"
+# Splunk 9.4+
+# Microk8s 1.32+
+# Docker.io
+# Redis server, tools & nano
+# pip libraries for Redis & Splunk
 
-if test -f "/opt/splukn/bin"; then
-  echo "Splunk is installed."
-else
-  echo "Please install Splunk first"
+if
+  version=$(sudo /opt/splunk/bin/splunk --version | awk '{print $2}' &&
+    [[ -n $version ]] &&
+    is-at-least 9.4 $version
+then
+  echo "FishStix requires the following; Splunk, Docker, Redis-Server/Tools & pip redis and Splunk libraries"
+  echo "Installing Docker, Redis and pip"
+  sudo apt install nano
+  sudo apt install docker.io
+  sudo apt install redis-server
+  sudo apt install redis-tools
+  sudo pip install redis splunklib splunk-sdk
 fi
+  echo "upgrade Splunk to version 9.4+"
 
-#Install the additional required software
-echo "Install Docker, Redis server, pip redis"
-sudo apt install nano
-sudo apt install docker.io
-sudo apt install redis-server
-sudo apt install redis-tools
-sudo pip install redis splunklib splunk-sdk
 
-echo "Running Microk8s installer"
